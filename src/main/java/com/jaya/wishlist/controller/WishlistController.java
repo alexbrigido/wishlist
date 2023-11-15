08365d762d9e4d23f0144edb4fbc208a601909b1
ALEXANDRE DIAS BRIGIDO <alexandre.brigido@spread.com.br>
2023-11-14 21:32:20 -0300

    initial commit

--- src/main/java/com/jaya/wishlist/controller/WishlistController.java
+++ src/main/java/com/jaya/wishlist/controller/WishlistController.java
@@ -1,2 +1,35 @@
-package com.jaya.wishlist.controller;public class WishlistController {
+package com.jaya.wishlist.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WishlistController {
+
+
+    @GetMapping(value = "/wishlist")
+    public ResponseEntity<Object> findAll(
+            @RequestParam(value = "clientId") Long clientId
+    ){
+        var response = "olá";
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/wishlist/{productId}")
+    public ResponseEntity<Object> findByProductId(
+            @RequestParam(value = "clientId") Long clientId,
+            @PathVariable(value = "productId") Long productId
+    ){
+        var response = "olá";
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = "/wishlist")
+    public ResponseEntity<Object> add(
+            @RequestParam(value = "clientId") Long clientId,
+            @RequestParam(value = "productId") Long productId
+    ){
+        var response = "olá";
+        return ResponseEntity.ok(response);
+    }
 }
